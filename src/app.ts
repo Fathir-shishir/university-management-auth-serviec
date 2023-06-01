b0468874c455a977f33aa4ca47ce2e201f9e7ed8
unknown <mfa.shishir@gmail.com>
2023-06-01 22:35:46 +0200

    setup eslist  preti list staged

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,16 @@
-import express, { Application, Request, Response } from "express"
+import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
-const app : Application= express()
-const port = 3000
+const app: Application = express()
 
 app.use(cors())
 
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 
-// testing 
+// testing
 
-app.get('/', (req : Request, res: Response) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
 })
 
-export default app;
-
+export default app
